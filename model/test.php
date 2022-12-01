7901af2133dcaad476d3c8230f21b6d27f864237
Taufiq <taofeekadekunle2@gmail.com>
2022-12-01 16:05:29 +0100

    Add code to test the response file on the test.php file

--- model/test.php
+++ model/test.php
@@ -1,14 +1,22 @@
 <?php
 
-require_once('Product.php');
-$size = array("large", "medium");
-$color = array("green", "blue");
+// require_once('Product.php');
+// $size = array("large", "medium");
+// $color = array("green", "blue");
 
-try{
+// try{
 
-    $product = new Product(1, "Shirt", "1000", $size, $color, "Description here", "picture123kj.jpg");
-    header("Content-type: application/json;charset=UTF-8");
-    echo json_encode($product->returnProductAsArray()); 
-}catch(ProductException $ex){
-    echo "Error: ".$ex->getMessage();
-}
+//     $product = new Product(1, "Shirt", "1000", $size, $color, "Description here", "picture123kj.jpg");
+//     header("Content-type: application/json;charset=UTF-8");
+//     echo json_encode($product->returnProductAsArray()); 
+// }catch(ProductException $ex){
+//     echo "Error: ".$ex->getMessage();
+// }
+
+require_once('Response.php');
+
+$response = new Response();
+$response->setSuccess(true);
+$response->setHttpStatusCode(200);
+$response->addMessage("Test Message 1");
+$response->send();
